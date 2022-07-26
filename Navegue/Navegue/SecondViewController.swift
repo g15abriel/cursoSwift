c195625d0f4b95884d122b32490c0f7d568326fa
ICMMAC09-BF7F <gabrielfelipe015@gmail.com>
2022-07-26 11:16:29 -0400

    app Navegue

---
+++ Navegue/Navegue/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Navegue
+//
+//  Created by ICMMAC09-BF7F on 26/07/22.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func returnSecondView(segue: UIStoryboardSegue){}
+
+}
---
+++ Navegue/Navegue/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Navegue
+//
+//  Created by ICMMAC09-BF7F on 26/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func returnMainView(segue: UIStoryboardSegue){}
+
+
+}
+
